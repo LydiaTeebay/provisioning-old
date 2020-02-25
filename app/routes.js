a507b99a65fd3c5c9c2a69b89f0c931dd51de14b
Lydia Teebay <lydia@hippodigital.co.uk>
2020-02-25 15:04:47 +0000

    adding notify api key back in

--- app/routes.js
+++ app/routes.js
@@ -5,12 +5,8 @@ const router = express.Router();
 
 // connect to Notify
 
-/* // Create new variables
-const notify = require('notifications-node-client').NotifyClient;
-const request = require('request');
-
-// Notify integration
-const notifyClient = new notify(process.env.NOTIFYAPIKEY); */
+var NotifyClient = require('notifications-node-client').NotifyClient,
+    notify = new NotifyClient(process.env.NOTIFYAPIKEY);
 
 
 /* router.post('/createaccount/v1/createaccount/setup/verification', function (req, res) {
